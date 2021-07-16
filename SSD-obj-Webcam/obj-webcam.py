e1942097b26e76546d3ae9a747d5a674f92e6df1
AbhirupDatta04 <66831444+AbhirupDatta04@users.noreply.github.com>
2021-07-16 12:09:46 +0530

    Webcam object detection
    Done with image classification over webcam interface

---
+++ SSD-obj-Webcam/obj-webcam.py
@@ -0,0 +1,36 @@
+import cv2
+import matplotlib.pyplot as plt
+config_file = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+frozen_model = 'frozen_inference_graph.pb'
+model = cv2.dnn_DetectionModel(frozen_model,config_file)
+classLabels=[]
+file_name ='Labels.txt'
+with open(file_name,'rt') as fpt:
+    classLabels = fpt.read().rstrip('\n').split('\n')
+model.setInputSize(320,320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5,127.5,127.5))
+model.setInputSwapRB(True)
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    cap=cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError('cannot open video')
+font_scale = 1
+font = cv2.FONT_HERSHEY_COMPLEX
+
+while True:
+    ret,frame = cap.read()
+    ClassIndex,confidence,bbox = model.detect(frame,confThreshold=0.6)
+    print(ClassIndex)
+    if (len(ClassIndex)!=0):
+        for ClassInd,conf,boxes in zip(ClassIndex.flatten(),confidence.flatten(),bbox):
+            if(ClassInd<=80):
+                cv2.rectangle(frame,boxes,(255,0,0),2)
+                cv2.putText(frame,classLabels[ClassInd-1],(boxes[0],boxes[1]),font,fontScale=font_scale,color=(0,255,0),thickness=1)
+    cv2.imshow('result video',frame)
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
